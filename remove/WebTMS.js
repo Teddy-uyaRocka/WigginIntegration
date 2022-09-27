d296dfc685dd035dbece61a125d6c313c4a5c05d
Teddy-uyaRocka <71017552+Teddy-uyaRocka@users.noreply.github.com>
2022-09-27 17:51:12 +0200

    Initial Commit

---
+++ remove/WebTMS.js
@@ -0,0 +1,296 @@
+var WebTMSData = [
+ // {
+ //   "DOC": {
+ //     "mark": [
+        {
+          "trademarkid": "86842985",
+          "recordownerid": "843",
+          "ownershipid": "1797082",
+          "trademarkprofilerecordownerid": "843",
+          "trademark": "1 VS 100 (b&w) (logo)",
+          "tmlogo": "87215361.jpg",
+          "addressofrecord": "",
+          "country": "WIPO",
+          "applicationno": "IR/938019",
+          "applicationdate": "2007-08-01T00:00:00",
+          "registrationno": "IR/938019",
+          "registrationdate": "2007-08-01T00:00:00",
+          "trademarkstatus": "Registered",
+          "filereference": "102816.000010",
+          "nextrenewaldue": "2027-08-01T00:00:00",
+          "typeofregistration": "IR Madrid",
+          "supervisor": "John Colbourn",
+          "supervisor2": "Mikah Pajaczkowska-Russell",
+          "supervisor3": "Akber Ahmed",
+          "supervisor4": "Mark Sylvester",
+          "active": "1",
+          "prioritydate": "2007-06-29T00:00:00",
+          "priorityappno": "827492",
+          "notes": "Chiever Ref No. TM6478WO00\nOld Chiever Ref No. T8061186MAD",
+          "hksection18": "0",
+          "recordcreated": "2018-10-29T15:55:35.420",
+          "recordcreatedby": "WigginConversionOct18",
+          "recordupdated": "2022-05-31T16:24:30.320",
+          "recordupdatedby": "Lorraine (CB)",
+          "trademarkprofileid": "46105289",
+          "typeoftrademark": "Word-Device",
+          "trademarkgroup": "(TMG/ESG) 1 vs 100",
+          "ownerid": "20724032",
+          "owner": "Endemol Nederland BV",
+          "owneraddress1": "MediArena 2",
+          "ownercity": "NL-1099 CZ Amsterdam",
+          "ownercountry": "Netherlands",
+          "clientid": "20853269",
+          "client": "Endemol Shine IP B.V.",
+          "clientaddress1": "MediArena 1",
+          "clientaddress2": "1114 BC",
+          "clientcity": "Amsterdam",
+          "clientcountry": "Netherlands",
+          "clientfilereference": "CLIENT",
+          "clientownersame": "0",
+          "goods": [
+            {
+              "trademarkid": "86842985",
+              "intclass": "9",
+              "goods": "Amusement machines and slot machines, adapted for use with an external display screen or monitor; gambling machines; videotapes; CD's; CD ROM's, DVD's and other sound- and image recording discs; computer games; electronic games, not included in other classes; downloadable internet games for mobile phones; electronic publications, interactive and non-interactive."
+            },
+            {
+              "trademarkid": "86842985",
+              "intclass": "16",
+              "goods": "Scratch cards."
+            },
+            {
+              "trademarkid": "86842985",
+              "intclass": "25",
+              "goods": "Clothing, footwear, headgear."
+            },
+            {
+              "trademarkid": "86842985",
+              "intclass": "28",
+              "goods": "Amusement machines and slot machines, automatic and coin-operated; amusement machines and slot machines, other than those adapted for use with an external display screen or monitor; games and playthings; games, including games of skill, games played with cards, hand-operated games, question and answer games, games of intelligence; games in the form of puzzles, word games and trivia and grammar games; board games; handheld games; electronic amusement games (other than those used exclusively with television receivers)."
+            },
+            {
+              "trademarkid": "86842985",
+              "intclass": "41",
+              "goods": "Education; entertainment like gambling; production of television and radio programmes and entertainment programmes; production of motion pictures, theatre productions and live performances; production of musicals; organization of music events and concerts; production of games, like gaming, via Internet; providing of games, like gaming, via Internet."
+            }
+          ],
+          "actions": [
+            {
+              "trademarkid": "86842985",
+              "duedate": "01 Feb 2027",
+              "action": "6 mo - Renewal Due",
+              "attorney": "Erin Meakin",
+              "complete": "0"
+            },
+            {
+              "trademarkid": "86842985",
+              "duedate": "01 Aug 2027",
+              "action": "Next Renewal Due",
+              "attorney": "Erin Meakin",
+              "complete": "0"
+            }
+          ],
+          "businessgroup": [
+            {
+              "businessgroupid": "2517",
+              "trademarkid": "86842985",
+              "recordownerid": "843",
+              "businessgroup": "(TMG/ESG) 1 vs 100"
+            },
+            {
+              "businessgroupid": "2411",
+              "trademarkid": "86842985",
+              "recordownerid": "843",
+              "businessgroup": "EndemolShine (EQ)"
+            }
+          ],
+          "tmlogouri": "/images/Pics.aspx?documentname=843/87215361.jpg"
+        },
+        {
+          "trademarkid": "86842991",
+          "recordownerid": "843",
+          "ownershipid": "1797088",
+          "trademarkprofilerecordownerid": "843",
+          "trademark": "1 VS 100 (b&w) (logo)",
+          "tmlogo": "87215361.jpg",
+          "addressofrecord": "",
+          "country": "Russian Federation",
+          "applicationno": "IR/938019",
+          "applicationdate": "2010-12-16T00:00:00",
+          "registrationno": "IR/938019",
+          "registrationdate": "2010-12-16T00:00:00",
+          "trademarkstatus": "Registered",
+          "filereference": "102816.000010",
+          "typeofregistration": "IR National Extension",
+          "supervisor": "John Colbourn",
+          "supervisor2": "Mikah Pajaczkowska-Russell",
+          "supervisor3": "Akber Ahmed",
+          "supervisor4": "Mark Sylvester",
+          "active": "1",
+          "prioritydate": "2007-06-29T00:00:00",
+          "priorityappno": "827492",
+          "hometrademarkid": "86842985",
+          "homeregdate": "2007-08-01T00:00:00",
+          "homeregno": "938019",
+          "homecountry": "WIPO",
+          "notes": "Chiever Ref No. TM6478RUWO\nOld Chiever Ref No. T8061186MAD",
+          "hksection18": "0",
+          "recordcreated": "2018-10-29T15:55:35.420",
+          "recordcreatedby": "WigginConversionOct18",
+          "recordupdated": "2022-05-31T16:24:30.320",
+          "recordupdatedby": "Lorraine (CB)",
+          "trademarkprofileid": "46105289",
+          "typeoftrademark": "Word-Device",
+          "trademarkgroup": "(TMG/ESG) 1 vs 100",
+          "ownerid": "20724032",
+          "owner": "Endemol Nederland BV",
+          "owneraddress1": "MediArena 2",
+          "ownercity": "NL-1099 CZ Amsterdam",
+          "ownercountry": "Netherlands",
+          "clientid": "20853269",
+          "client": "Endemol Shine IP B.V.",
+          "clientaddress1": "MediArena 1",
+          "clientaddress2": "1114 BC",
+          "clientcity": "Amsterdam",
+          "clientcountry": "Netherlands",
+          "clientfilereference": "CLIENT",
+          "clientownersame": "0",
+          "goods": [
+            {
+              "trademarkid": "86842991",
+              "intclass": "9",
+              "goods": "Amusement machines and slot machines, adapted for use with an external display screen or monitor; gambling machines; videotapes; CD's; CD ROM's, DVD's and other sound- and image recording discs; computer games; electronic games, not included in other classes; downloadable internet games for mobile phones; electronic publications, interactive and non-interactive."
+            },
+            {
+              "trademarkid": "86842991",
+              "intclass": "16",
+              "goods": "Scratch cards."
+            },
+            {
+              "trademarkid": "86842991",
+              "intclass": "25",
+              "goods": "Clothing, footwear, headgear."
+            },
+            {
+              "trademarkid": "86842991",
+              "intclass": "28",
+              "goods": "Amusement machines and slot machines, automatic and coin-operated; amusement machines and slot machines, other than those adapted for use with an external display screen or monitor; games and playthings; games, including games of skill, games played with cards, hand-operated games, question and answer games, games of intelligence; games in the form of puzzles, word games and trivia and grammar games; board games; handheld games; electronic amusement games (other than those used exclusively with television receivers)."
+            },
+            {
+              "trademarkid": "86842991",
+              "intclass": "41",
+              "goods": "Education; entertainment like gambling; production of television and radio programmes and entertainment programmes; production of motion pictures, theatre productions and live performances; production of musicals; organization of music events and concerts; production of games, like gaming, via Internet; providing of games, like gaming, via Internet."
+            }
+          ],
+          "businessgroup": [
+            {
+              "businessgroupid": "2517",
+              "trademarkid": "86842991",
+              "recordownerid": "843",
+              "businessgroup": "(TMG/ESG) 1 vs 100"
+            },
+            {
+              "businessgroupid": "2411",
+              "trademarkid": "86842991",
+              "recordownerid": "843",
+              "businessgroup": "EndemolShine (EQ)"
+            }
+          ],
+          "tmlogouri": "/images/Pics.aspx?documentname=843/87215361.jpg"
+        },
+        {
+          "trademarkid": "86842990",
+          "recordownerid": "843",
+          "ownershipid": "1797087",
+          "trademarkprofilerecordownerid": "843",
+          "trademark": "1 VS 100 (b&w) (logo)",
+          "tmlogo": "87215361.jpg",
+          "addressofrecord": "",
+          "country": "EUTM",
+          "applicationno": "IR/938019",
+          "applicationdate": "2010-12-16T00:00:00",
+          "registrationno": "IR/938019",
+          "registrationdate": "2010-12-16T00:00:00",
+          "trademarkstatus": "Registered",
+          "filereference": "102816.000010",
+          "typeofregistration": "EU Trademark",
+          "supervisor": "John Colbourn",
+          "supervisor2": "Mikah Pajaczkowska-Russell",
+          "supervisor3": "Akber Ahmed",
+          "supervisor4": "Mark Sylvester",
+          "active": "1",
+          "prioritydate": "2007-06-29T00:00:00",
+          "priorityappno": "827492",
+          "hometrademarkid": "86842985",
+          "homeregdate": "2007-08-01T00:00:00",
+          "homeregno": "938019",
+          "homecountry": "WIPO",
+          "notes": "Chiever Ref No. TM6478EUWO\nOld Chiever Ref No. T8061186MAD",
+          "hksection18": "0",
+          "recordcreated": "2018-10-29T15:55:35.420",
+          "recordcreatedby": "WigginConversionOct18",
+          "recordupdated": "2022-05-31T16:24:30.320",
+          "recordupdatedby": "Lorraine (CB)",
+          "trademarkprofileid": "46105289",
+          "typeoftrademark": "Word-Device",
+          "trademarkgroup": "(TMG/ESG) 1 vs 100",
+          "ownerid": "20724032",
+          "owner": "Endemol Nederland BV",
+          "owneraddress1": "MediArena 2",
+          "ownercity": "NL-1099 CZ Amsterdam",
+          "ownercountry": "Netherlands",
+          "clientid": "20853269",
+          "client": "Endemol Shine IP B.V.",
+          "clientaddress1": "MediArena 1",
+          "clientaddress2": "1114 BC",
+          "clientcity": "Amsterdam",
+          "clientcountry": "Netherlands",
+          "clientfilereference": "CLIENT",
+          "agentid": "20765440",
+          "agent": "Wiggin LLP Brussels",
+          "agentcountry": "EUTM",
+          "clientownersame": "0",
+          "goods": [
+            {
+              "trademarkid": "86842990",
+              "intclass": "9",
+              "goods": "Amusement machines and slot machines, adapted for use with an external display screen or monitor; gambling machines; videotapes; CD's; CD ROM's, DVD's and other sound- and image recording discs; computer games; electronic games, not included in other classes; downloadable internet games for mobile phones; electronic publications, interactive and non-interactive."
+            },
+            {
+              "trademarkid": "86842990",
+              "intclass": "16",
+              "goods": "Scratch cards."
+            },
+            {
+              "trademarkid": "86842990",
+              "intclass": "25",
+              "goods": "Clothing, footwear, headgear."
+            },
+            {
+              "trademarkid": "86842990",
+              "intclass": "28",
+              "goods": "Amusement machines and slot machines, automatic and coin-operated; amusement machines and slot machines, other than those adapted for use with an external display screen or monitor; games and playthings; games, including games of skill, games played with cards, hand-operated games, question and answer games, games of intelligence; games in the form of puzzles, word games and trivia and grammar games; board games; handheld games; electronic amusement games (other than those used exclusively with television receivers)."
+            }
+          ],
+          "businessgroup": [
+            {
+              "businessgroupid": "2517",
+              "trademarkid": "86842990",
+              "recordownerid": "843",
+              "businessgroup": "(TMG/ESG) 1 vs 100"
+            },
+            {
+              "businessgroupid": "2411",
+              "trademarkid": "86842990",
+              "recordownerid": "843",
+              "businessgroup": "EndemolShine (EQ)"
+            }
+          ],
+          "tmlogouri": "/images/Pics.aspx?documentname=843/87215361.jpg"
+        }
+ //     ]
+ //   }
+ // }
+
+  ]
